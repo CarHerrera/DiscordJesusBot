4947c58e5db9e6f4431e6fa8aacaa19431dc4f7d
Carlos <herrera4345@gmail.com>
2021-03-12 19:44:43 -0500

    forgot to update vars

--- cogs/admin.py
+++ cogs/admin.py
@@ -3,7 +3,7 @@ import json
 from random import randint
 from discord.ext import commands, tasks
 from datetime import datetime
-from bot import timeChecker
+from main import timeChecker
 import pandas, csv
 file = open("./settings/good_noodle.txt")
 data = file.read()
